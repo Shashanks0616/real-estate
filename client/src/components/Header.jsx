bc7e1777b3c6fd478d443f19f1be4e2c4834f11e
Shashank <shashanksingh0616@gmail.com>
2023-10-09 17:34:42 +0530

    Add the logo and complete the about section of the client section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,12 +25,20 @@ export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <Link to='/'>
-          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
-          </h1>
+  <Link to='/' className='flex items-center'>
+  <img
+      src="shashank.png"
+      alt="shashank's-estates"
+      className='w-10 h-10 rounded-full ml-0 mr-2 overflow-hidden inline-block'
+    />
+    <h1 className='font-bold text-sm sm:text-xl cursor-pointer'>
+      <span className='text-slate-500'>Shashank's</span>
+      <span className='text-slate-700'>Estate</span>
+    </h1>
+    
+
         </Link>
+        
         <form
           onSubmit={handleSubmit}
           className='bg-slate-100 p-3 rounded-lg flex items-center'
@@ -70,6 +78,8 @@ export default function Header() {
           </Link>
         </ul>
       </div>
+      
     </header>
+    
   );
 }
